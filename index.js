d2e605a9e935f73d1d1b1dc2a509bb06daa65f3c
+ <+>
2021-08-31 14:33:48 -0700

    Map and reduce lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(array, fn) {
+    const newArray = [];
+    array.forEach(int => newArray.push(fn(int)));
+    return newArray;
+}
+
+function reduce(array, fn, startingPoint) {
+    let newTotal;
+    if (startingPoint) {
+        newTotal = startingPoint;
+        for (let i = 0; i < array.length; i++) {
+            newTotal = fn(array[i], newTotal);
+        }
+        // array.forEach(element => newTotal += fn(element, newTotal));
+        // debugger;
+        return newTotal;
+    } else {
+        newTotal = array[0];
+        for (let i = 1; i < array.length; i++) {
+            newTotal = fn(array[i], newTotal);
+        }
+        return newTotal;
+    }
+}
